ad599ca7f56ba31585545b8022bb304ad370004b
a.sapozhkov <a@sapozhkov.ru>
2024-03-06 19:35:18 +0300

    v2 добавлен сброс заявок в случае отсутствия активности в течение 10 минут

--- trader_bot_v2.py
+++ trader_bot_v2.py
@@ -2,12 +2,15 @@
 Вторая версия скальпинг бота
 
 Недостатки предыдущей версии:
-    1. продает и тут же покупает обратно за ту же цену
-    2. редко торгует (раз в час/два)
+    1. V продает и тут же покупает обратно за ту же цену - исправлено
+    2. ~ редко торгует (раз в час/два) - в процессе
+
+Недостатки этой
+    1. раз в 10 минут проводить
 
 Задача:
-    1. Имплемент предыдущей
-    2. Учимся покупать внизу, а продавать вверху
+    1. V Имплемент предыдущей
+    2. O Учимся покупать внизу, а продавать вверху
 
 Логика v2:
     1. состояние анализа
@@ -62,7 +65,7 @@ class ScalpingBot:
     STATE_HAS_1 = 1
     STATE_HAS_2 = 2
 
-    def __init__(self, token, figi, account_id, profit_percent=0.15, stop_loss_percent=1.0):
+    def __init__(self, token, figi, account_id, profit_percent=0.13, stop_loss_percent=1.0):
         self.commission = 0.0005
         self.token = token
         self.figi = figi
@@ -83,11 +86,17 @@ class ScalpingBot:
         order_book = self.fetch_order_book()
         self.last_price = self.quotation_to_float(order_book.last_price)
 
+        self.last_successful_operation_time = datetime.now(timezone.utc)
+        self.reset_last_operation_time()
+
         self.buy_order = None
         self.sell_order = None
 
         self.logger.info('INIT')
         self.logger.info(f"FIGI - {self.figi}")
+
+    def reset_last_operation_time(self):
+        self.last_successful_operation_time = datetime.now(timezone.utc)
 
     @staticmethod
     def quotation_to_float(quotation):
@@ -225,6 +234,40 @@ class ScalpingBot:
 
         return forecast_low, forecast_high
 
+    def change_state_bought(self):
+        self.state = min(self.STATE_HAS_2, self.state + 1)
+
+    def change_state_sold(self):
+        self.state = max(self.STATE_HAS_0, self.state - 1)
+
+    def can_buy(self):
+        return self.state != self.STATE_HAS_2
+
+    def can_sell(self):
+        return self.state != self.STATE_HAS_0
+
+    def cancel_active_orders(self):
+        """Отменяет все активные заявки."""
+        with Client(self.token) as client:
+            if self.buy_order:
+                client.orders.cancel_order(account_id=self.account_id, order_id=self.buy_order.order_id)
+                self.logger.info(f"Buy order {self.buy_order.order_id} canceled")
+
+            if self.sell_order:
+                client.orders.cancel_order(account_id=self.account_id, order_id=self.sell_order.order_id)
+                self.logger.info(f"Sell order {self.sell_order.order_id} canceled")
+
+            self.reset_last_operation_time()
+
+    def check_and_cansel_orders(self):
+        """Сбрасываем активные заявки, если не было активных действий за последнее время"""
+        no_operation_timeout_seconds = 600  # 10 минут = 600 секунд
+        current_time = datetime.now(timezone.utc)
+        if (current_time - self.last_successful_operation_time).total_seconds() > no_operation_timeout_seconds:
+            self.logger.info(f"{no_operation_timeout_seconds/60} минут без активности. Снимаем и переставляем заявки.")
+            self.cancel_active_orders()
+            self.reset_last_operation_time()
+
     def run(self):
         while True:
             if not self.can_trade():
@@ -238,6 +281,7 @@ class ScalpingBot:
                 self.logger.info(f"BUY order executed, price {self.buy_order.initial_order_price}")
                 self.change_state_bought()
                 self.buy_order = None
+                self.reset_last_operation_time()
 
             # отслеживаем исполнение заявки на продажу
             if self.sell_order and self.order_is_executed(self.sell_order):
@@ -245,6 +289,10 @@ class ScalpingBot:
                 self.logger.info(f"SELL order executed, price {self.sell_order.initial_order_price}")
                 self.change_state_sold()
                 self.sell_order = None
+                self.reset_last_operation_time()
+
+            # сбрасываем активные заявки, если не было активных действий за последнее время
+            self.check_and_cansel_orders()
 
             # если нет хоть одной заявки
             if not self.buy_order or not self.sell_order:
@@ -252,35 +300,28 @@ class ScalpingBot:
                 # прикидываем цены
                 last_candles = self.fetch_candles()
                 forecast_low, forecast_high = self.forecast_next_candle(last_candles)
-                diff = forecast_high - forecast_low
+                diff = round(forecast_high - forecast_low, 2)
 
                 order_book = self.fetch_order_book()
                 self.update_values(order_book)
 
                 # проверяем что есть смысл торговать на таком диапазоне цен
-                if diff >= self.last_price * self.profit_percent:
+                need_profit = round(self.last_price * self.profit_percent, 2)
+                if diff >= need_profit:
                     if not self.buy_order and self.can_buy():
-                        self.logger.debug(f"Заявка на покупку по  {forecast_low}")
+                        self.logger.debug(f"Размещена заявка на покупку по {forecast_low}")
                         self.buy_order = self.buy_limit(forecast_low)
 
                     if not self.sell_order and self.can_sell():
-                        self.logger.debug(f"Заявка на продажу по  {forecast_high}")
+                        self.logger.debug(f"Размещена заявка на продажу по {forecast_high}")
                         self.sell_order = self.sell_limit(forecast_high)
 
-            self.logger.debug(f"отторговали, ждем следующего цикла, sleep {self.sleep_no_trade}")
+                else:
+                    self.logger.info(f"Пока не торгуем. "
+                                     f"Ожидаемая разница в торгах - {diff}, а требуется минимум {need_profit} ")
+
+            self.logger.debug(f"Ждем следующего цикла, sleep {self.sleep_no_trade}")
             time.sleep(self.sleep_trading)
-
-    def change_state_bought(self):
-        self.state = min(self.STATE_HAS_2, self.state + 1)
-
-    def change_state_sold(self):
-        self.state = max(self.STATE_HAS_0, self.state - 1)
-
-    def can_buy(self):
-        return self.state != self.STATE_HAS_2
-
-    def can_sell(self):
-        return self.state != self.STATE_HAS_0
 
 
 bot = ScalpingBot(TOKEN, FIGI, ACCOUNT_ID)
