9b552d828ea589f94b760ea70a2d1167ad4fea1a
a.sapozhkov <a@sapozhkov.ru>
2024-03-28 19:39:53 +0300

    доработан прогресс бар - вывод времени

--- lib/task_progress.py
+++ lib/task_progress.py
@@ -29,9 +29,26 @@ class TaskProgress:
         # Очистка текущей строки в консоли и вывод прогресс-бара
         print('\r', end='')
         print(f'[{bar}] {percent_complete:.0f}% ({self.current_iteration}/{self.total_iterations}), '
-              f'закончим в ~ {estimated_end_time.strftime("%H:%M")}', end='')
+              f'закончим через {self.get_remaining_time_text(remaining_time)} '
+              f'в {estimated_end_time.strftime("%H:%M")}', end='')
 
         if self.current_iteration == self.total_iterations:
             print('\r' + ' ' * 100, end='\r')
             duration = timedelta(seconds=round(elapsed_time))
             print(f'Закончено в {datetime.now().strftime("%H:%M")}, длительность {duration}', end='\n')
+
+    @staticmethod
+    def get_remaining_time_text(remaining_time):
+        # Преобразуем секунды в часы, минуты и секунды
+        hours, remainder = divmod(remaining_time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        # Формируем строку для вывода с учетом наличия часов
+        parts = []
+        if hours > 0:
+            parts.append(f"{int(hours):02}ч")
+        if minutes > 0 or hours > 0:  # Выводим минуты, если есть часы или минуты
+            parts.append(f"{int(minutes):02}м")
+        parts.append(f"{int(seconds):02}с")
+
+        return " ".join(parts[:2]) + ' ' * 10
