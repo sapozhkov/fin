e08f30c951e78f1a1d629a9b0c296cef979eb546
a.sapozhkov <a@sapozhkov.ru>
2024-05-20 22:23:53 +0300

    #114 основа для миграций

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     TOKEN = os.environ.get('INVEST_TOKEN')
     PASSWORD = os.environ.get('PASSWORD')
     DEBUG_MODE = True if os.environ.get('DEBUG') == '1' else False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/trading_bot.db'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db', 'db.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
