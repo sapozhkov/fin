cc8554d03f1b3a10717996011b57d1c501178d08
a.sapozhkov <a@sapozhkov.ru>
2024-05-29 22:27:53 +0300

    #120 добавлена защита от перебора паролей

--- app/routes/common.py
+++ app/routes/common.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime
+
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import current_user, login_user, logout_user
 from app.models import User
@@ -5,18 +8,40 @@ from config import Config
 
 bp = Blueprint('common', __name__)
 
+LOG_FILE = f"{Config.BASE_DIR}/log/login_attempts.log"
+
+
+def log_failed_attempt(password):
+    with open(LOG_FILE, 'a') as log:
+        log.write(f'{datetime.now()}, {password}\n')
+
+
+def is_locked():
+    if not os.path.exists(LOG_FILE):
+        return False
+    with open(LOG_FILE, 'r') as log:
+        attempts = log.readlines()
+        return len(attempts) >= 300
+
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('admin.index'))
+
     if request.method == 'POST':
+        if is_locked():
+            flash('Account locked due to too many failed login attempts. Try again later.')
+            return redirect(url_for('common.login'))
+
         password = request.form.get('password')
         if password == Config.PASSWORD:
             user = User()
             login_user(user)
+            open(LOG_FILE, 'w').close()
             return redirect(url_for('admin.index'))
         else:
+            log_failed_attempt(password)
             flash('Invalid password.')
     return render_template('login.html')
 
