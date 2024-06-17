cac69b5e405b54730e43a5175306e80a6e7a269a
a.sapozhkov <a@sapozhkov.ru>
2024-06-18 02:18:32 +0300

    #177 удалил индекс для баланса 2

--- app/models/acc_run_balance.py
+++ app/models/acc_run_balance.py
@@ -5,11 +5,9 @@ class AccRunBalance(db.Model):
     __tablename__ = 'acc_run_balance'
 
     id = db.Column(db.Integer, primary_key=True)
-    acc_run = db.Column(db.Integer, db.ForeignKey('acc_runs.id', name='fk_acc_run_balance_acc_run'), nullable=False)
+    acc_run = db.Column(db.Integer, nullable=False)
     balance = db.Column(db.Float)
     datetime = db.Column(db.DateTime)
-
-    acc_run_rel = db.relationship('AccRun')
 
     def __repr__(self):
         return f'<AccRunBalance {self.datetime} {self.balance}>'
