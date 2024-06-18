ecc59f9bf8640f87784f5edf78f0445cde7dd114
a.sapozhkov <a@sapozhkov.ru>
2024-06-18 23:46:51 +0300

    TaskStatus - выпадающий список для редактирования

--- app/constants/task_status.py
+++ app/constants/task_status.py
@@ -7,3 +7,17 @@ class TaskStatus:
     @classmethod
     def get_closed_statuses(cls):
         return [cls.FINISHED, cls.FAILED]
+
+    @classmethod
+    def get_list(cls):
+        return [
+            (cls.PENDING, 'Pending'),
+            (cls.IN_PROGRESS, 'In progress'),
+            (cls.FINISHED, 'Finished'),
+            (cls.FAILED, 'Failed'),
+        ]
+
+    @classmethod
+    def get_title(cls, status):
+        status_dict = dict(cls.get_list())
+        return status_dict.get(status, 'Unknown')
--- web/views/task_view.py
+++ web/views/task_view.py
@@ -1,5 +1,6 @@
 from flask_admin.contrib.sqla import ModelView
 
+from app.constants import TaskStatus
 from web.formater import view_format_datetime, view_format_class_name
 
 
@@ -11,3 +12,5 @@ class TaskView(ModelView):
         'created_at': view_format_datetime,
         'updated_at': view_format_datetime,
     }
+    form_choices = {'status': TaskStatus.get_list()}
+    column_choices = {'status': TaskStatus.get_list()}
