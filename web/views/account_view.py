d6b493ae16a0efafa4b607422c00287c0f81926a
a.sapozhkov <a@sapozhkov.ru>
2024-06-19 02:13:17 +0300

    #186 добавлена валидация конфига в форму сохранения аккаунта

--- web/views/account_view.py
+++ web/views/account_view.py
@@ -1,5 +1,7 @@
 from flask_admin.contrib.sqla import ModelView
+from wtforms import ValidationError
 
+from app.config import AccConfig
 from web.formater import view_format_datetime
 
 
@@ -33,3 +35,12 @@ class AccountView(ModelView):
             (1, 'Active')
         ]
     }
+
+    def on_model_change(self, form, model, is_created):
+        try:
+            AccConfig.from_repr_string(model.config)
+        except ValueError as e:
+            raise ValidationError(e)
+
+        # Вызываем родительский метод для продолжения стандартной обработки
+        super(AccountView, self).on_model_change(form, model, is_created)
