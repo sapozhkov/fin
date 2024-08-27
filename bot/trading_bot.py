db5176b2cd375c1b48667c580b5ce3831e856022
a.sapozhkov <a@sapozhkov.ru>
2024-08-27 23:42:32 +0300

    #224 вернул обратно значение instrument_data

--- bot/trading_bot.py
+++ bot/trading_bot.py
@@ -70,7 +70,7 @@ class TradingBot(AbstractBot):
                 profit=0,
                 expected_profit=instrument.expected_profit,
                 data='',
-                instrument_data=f"{instrument.data}",
+                instrument_data=f"{instrument.data}, exp {instrument.expected_profit}",
                 config=str(self.config),
                 start_cnt=self.trade_strategy.start_count,
                 end_cnt=0,
