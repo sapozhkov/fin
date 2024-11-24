6442524301ffd35445e1be3092d84551532c0af6
a.sapozhkov <a@sapozhkov.ru>
2024-11-24 23:44:57 +0300

    #148 сортировка инструментов по умолчанию в админке сделана по -id

--- web/views/instrument_view.py
+++ web/views/instrument_view.py
@@ -11,7 +11,7 @@ from web.formater import view_format_datetime, view_format_currency, view_format
 
 class InstrumentView(ModelView):
     column_display_pk = True
-    column_default_sort = [('account', False), ('id', False)]
+    column_default_sort = [('id', True)]
     column_list = (
         'id',
         'profit_n_last_day',
