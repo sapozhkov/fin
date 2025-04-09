90101d97d2b37c3f75c987ee2de3148b85c9ca7d
a.sapozhkov <a@sapozhkov.ru>
2025-04-09 19:26:14 +0300

    #334 фикс сессий v2

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_session import Session
 
 from app.config import AppConfig
 
@@ -12,10 +13,13 @@ def create_app(config_class=AppConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    Session(app)
+
     # Инициализация расширений с приложением
     db.init_app(app)
     migrate.init_app(app, db)
 
     return app
+
 
 import app.models
